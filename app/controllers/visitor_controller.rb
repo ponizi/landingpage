83ef8aae349aa7f4c1b516f9a018f6339aed6c96
ponizi <ponalexis@gmail.com>
2019-11-19 14:35:54 +0100

    Change error message

--- app/controllers/visitor_controller.rb
+++ app/controllers/visitor_controller.rb
@@ -10,17 +10,16 @@ class VisitorController < ApplicationController
   
   def create
     @visitor = Visitor.new(first_name: visitor_params[:first_name], last_name: visitor_params[:last_name], phone_number: visitor_params[:phone_number], email: visitor_params[:email], message: visitor_params[:message])
-    if @visitor.save
-    flash[:success] = "Adresse e-mail enregistrée, vous recevrez l'actualité de Shomi prochainement"
-    else
+    a = is_known
       if @visitor.email.nil? 
       flash[:danger] = "Adresse email manquante"
-      elsif is_known == 1
+      elsif a == 1
       flash[:danger] = "Adresse email déjà enregistrée"
-      else 
-      flash[:danger] = "Format d'adresse email invalide"
+      elsif @visitor.save
+      flash[:success] = "Adresse e-mail enregistrée, vous recevrez l'actualité de Shomi prochainement"
+      else
+        flash[:danger] = "Adresse email invalide ou déjà enregistrée"
       end
-    end
    redirect_to root_path
   end
 
