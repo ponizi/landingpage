09eb0a69ac18f453ea8e7db6af32f87d2f4c2a23
ponizi <ponalexis@gmail.com>
2019-11-19 12:14:17 +0100

    Add validates email

--- app/controllers/visitor_controller.rb
+++ app/controllers/visitor_controller.rb
@@ -10,11 +10,17 @@ class VisitorController < ApplicationController
   
   def create
     @visitor = Visitor.new(first_name: visitor_params[:first_name], last_name: visitor_params[:last_name], phone_number: visitor_params[:phone_number], email: visitor_params[:email], message: visitor_params[:message])
-   if @visitor.save
-    flash[:success] = "Message Bien Envoyé !"
-   else
-    flash[:danger] = "Message non remis : Adresse email manquante"
-   end
+    if @visitor.save
+    flash[:success] = "Adresse e-mail enregistrée, vous recevrez l'actualité de Shomi prochainement"
+    else
+      if @visitor.email.nil? 
+      flash[:danger] = "Adresse email manquante"
+      elsif is_known == 1
+      flash[:danger] = "Adresse email déjà enregistrée"
+      else 
+      flash[:danger] = "Format d'adresse email invalide"
+      end
+    end
    redirect_to root_path
   end
 
@@ -22,5 +28,13 @@ class VisitorController < ApplicationController
     params.require(:visitor).permit(:first_name,:last_name,:phone_number,:email,:message)
   end
 
+  def is_known
+    a = 0
+    Visitor.all.each {|visitor| visitor.email == @visitor.email ? a = 1 : a = 0 }
+      puts "*"*60
+      puts "#{a}"
+      puts "*"*60
+    return a
+  end
 
 end
--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,3 +1,5 @@
 class Visitor < ApplicationRecord
   validates :email, presence:true
+  validates_uniqueness_of :email
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
