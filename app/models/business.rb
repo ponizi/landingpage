63781d9be2cfa206c9979c4839831109e84ad4c5
ponizi <ponalexis@gmail.com>
2019-12-04 12:31:58 +0100

    Add error message

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,2 +1,7 @@
-class Business < ApplicationRecord
+class Business < ApplicationRecord 
+  validates :email, presence:true
+  validates :name, presence:true
+  validates :phone_number, presence:true
+  validates_uniqueness_of :email
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
